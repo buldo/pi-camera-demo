ed481d24586d09f5f7da0cc19edbc91cd2185c82
Roman Buldygin <froooks@gmail.com>
2022-07-11 02:04:17 +0300

    Add project files.

---
+++ pi-camera-demo/pi-camera-demo.cpp
@@ -0,0 +1,12 @@
+﻿// pi-camera-demo.cpp : Defines the entry point for the application.
+//
+
+#include "pi-camera-demo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
